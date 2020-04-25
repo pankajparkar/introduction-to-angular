15caf193c8fd3b3a6bc66c1c116314c7cd238e85
Pankaj Parkar <parkarpankaj3@gmail.com>
2020-04-26 00:50:06 +0800

    Added Initial commit with Angular 9.

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ita-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'introduction-to-angular';
+}
